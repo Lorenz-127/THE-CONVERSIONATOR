17773c7248a7739fd5264306f9144630544cdf1a
Lorenz B. <lorenz.baechtold@web.de>
2024-05-17 22:02:49 +0000

    feat(js): Add event listener to initialize calculations when input fields change

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,27 @@ document.getElementById("curtain-close").addEventListener("click", () => {
     // reset blur effect to content behind curtain menu to hidden
     document.getElementById("blur-content").classList.remove("active");
 });
+
+// Add event listener to initialize calculations when input fields change
+document.addEventListener("input", function (event) {
+    if (
+        event.target.id === "velocity-field" ||
+        event.target.id === "distance-km-field"
+    ) {
+        // Format the input value before calculating time and distance
+        event.target.value = formatNumberForLocale(event.target.value);
+        calculateTimeAndDistance();
+    } else if (
+        event.target.id === "distance-fuel-field" ||
+        event.target.id === "distance-consumption-field" ||
+        event.target.id === "price-per-unit-field"
+    ) {
+        calculateFuelCost();
+    } else if (
+        event.target.id === "transportation-mode" ||
+        event.target.id === "fuel-type" ||
+        event.target.id === "distance-co2-footprint-field"
+    ) {
+        calculateCO2Footprint();
+    }
+});
