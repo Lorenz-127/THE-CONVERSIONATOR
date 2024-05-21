c373971b34b15620506ead9a2f349621d72c92e5
Lorenz-127 <lorenz.baechtold@web.de>
2024-05-21 14:18:45 +0000

    chore(js): Add test.js to run the refactored script.

---
+++ assets/js/test.js
@@ -0,0 +1,26 @@
+
+
+
+// function to calculate travel time
+    // t = dkm/v
+    // t = dmi/v
+
+// function to calculate distance in Km
+    // dkm = v/t
+    // dmi = v/t
+
+// function to calculate velocity
+    // v = dkm/t
+    // v = dmi/t
+
+// function to convert Km to Mi
+    // 1 km = 0.621371 mi
+
+// function to convert Mi to Km
+    // 1 mi = 1.60934 km
+
+// function to reset all travel time input fields
+    // set all input fields to 0
+
+// Event listener for reset button to execute resetTravelTimeFields
+    // add event listener to reset button to execute resetTravelTimeFields
