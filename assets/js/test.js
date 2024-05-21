d3954b04efa9587b33b5719b38447d2c3afa7a69
Lorenz-127 <lorenz.baechtold@web.de>
2024-05-21 16:35:13 +0000

    chore(js): Add helper comments for Functions travel time section to test.js

--- assets/js/test.js
+++ assets/js/test.js
@@ -125,22 +125,23 @@ document.addEventListener("DOMContentLoaded", () => {
 // function to calculate travel time with mi and velocity
 // t = dmi/v
 
-// function to calculate distance in Km
-    // dkm = v/t
-    // dmi = v/t
+// function to calculate distance in Km with velocity and time
+// dkm = v/t
 
-// function to calculate velocity
-    // v = dkm/t
-    // v = dmi/t
+// function to calculate distance in Mi with velocity and time
+// dmi = v/t
+
+// function to calculate velocity with km and time
+// v = dkm/t
+
+// function to calculate velocity with mi and time
+// v = dmi/t
 
 // function to convert Km to Mi
-    // 1 km = 0.621371 mi
+// 1 km = 0.621371 mi
 
 // function to convert Mi to Km
-    // 1 mi = 1.60934 km
+// 1 mi = 1.60934 km
 
 // function to reset all travel time input fields
-    // set all input fields to 0
-
-// Event listener for reset button to execute resetTravelTimeFields
-    // add event listener to reset button to execute resetTravelTimeFields
+// set all input fields to 0
