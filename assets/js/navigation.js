91505c7b653976e392db2434b86bf2c577d793c3
Lorenz-127 <lorenz.baechtold@web.de>
2024-05-27 22:42:17 +0000

    chore(js): change filename

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,125 +1,230 @@
 // Wait until the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", function () {
-    // Add event listener to element with ID "open-modal-menu"
-    document.getElementById("open-modal-menu").addEventListener("click", () => {
-        // Display the element with ID "modal-menu" when clicked
-        document.getElementById("modal-menu").style.display = "block";
-        // Set blur effect to content behind modal menu visible
-        document.getElementById("blur-content").classList.add("active");
-        // Disable body scroll
-        document.body.style.overflow = 'hidden';
-    });
-
-    // Add event listener to element with ID "close-modal"
-    document.getElementById("close-modal").addEventListener("click", () => {
-        // Hide the element with ID "modal-menu" when clicked
-        document.getElementById("modal-menu").style.display = "none";
-        // Reset blur effect to content behind modal menu to hidden
-        document.getElementById("blur-content").classList.remove("active");
-        // Enable body scroll
-        document.body.style.overflow = 'auto';
-    });
-});
-
-// Add variable for event listener to ID "open-instructions"
-const infoBtn = document.getElementById("open-instructions");
-// Add variable for event listener to links in overlay section
-const overlayNavLinks = document.querySelectorAll(".overlay-nav-links a");
-// Add variable for event listener to links in both overlay-nav-link and instructions-wrapper sections 
-const navLinks = document.querySelectorAll(".overlay-nav-links a, .instructions-wrapper a");
-
-
-// Add even listener to element with ID "open-instructions"
-infoBtn.addEventListener("click", function () {
-    // Hide all sections
-    hideAllSections();
-
-    // Show instructions section
-    openInstructionSection();
+  // Get event listeners
+  initializeModalMenu();
+  initializeInstructionButton();
+  initializeNavLinks();
+  handleInstructionLinkClicks();
+  updateNavLinks();
+  window.addEventListener("resize", updateNavLinks);
+  window.addEventListener("load", updateNavLinks);
 });
 
 /**
- * Function to open the Instructions via circle-question icon
+ * Function to add event listeners to open and close the modal menu.
  */
-function openInstructionSection() {
-    const instructionSection = document.getElementById("instructions");
-    instructionSection.style.display = "grid";
+function initializeModalMenu() {
+  const openButton = document.getElementById("open-modal-menu");
+  const closeButton = document.getElementById("close-modal-menu");
+  const modal = document.getElementById("modal-menu");
+  const blurContent = document.getElementById("blur-content");
+
+  if (openButton && closeButton && modal && blurContent) {
+    openButton.addEventListener("click", () => openModal(modal, blurContent));
+    closeButton.addEventListener("click", () => closeModal(modal, blurContent));
+  }
+  return { modal, blurContent };
 }
 
 /**
- * Function to hide all sections
+ * Open the modal menu.
+ */
+function openModal(modal, blurContent) {
+  modal.style.display = "block";
+  blurContent.classList.add("active");
+  document.body.style.overflow = "hidden";
+}
+
+/**
+ * Close the modal menu.
+ */
+function closeModal(modal, blurContent) {
+  modal.style.display = "none";
+  blurContent.classList.remove("active");
+  document.body.style.overflow = "auto";
+}
+
+/**
+ * Function to add event listener to the "open-instructions" button.
+ */
+function initializeInstructionButton() {
+  const infoBtn = document.getElementById("open-instructions");
+  if (infoBtn) {
+    infoBtn.addEventListener("click", function () {
+      hideAllSections();
+      openInstructionSection();
+    });
+  }
+}
+
+/**
+ * Hides all sections.
  */
 function hideAllSections() {
-    const sections = document.querySelectorAll("section");
-    sections.forEach((section) => {
-        section.style.display = "none";
-    });
+  const sections = document.querySelectorAll("section");
+  sections.forEach((section) => {
+    section.style.display = "none";
+  });
+}
+
+/**
+ * Opens the instructions section.
+ */
+function openInstructionSection() {
+  const instructionSection = document.getElementById("instructions");
+  if (instructionSection) {
+    instructionSection.style.display = "grid";
+  }
+}
+
+/**
+ * Function to add event listener to each overlay navigation link.
+ */
+function initializeNavLinks() {
+  const navLinks = document.querySelectorAll(".overlay-nav-links li");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", handleNavLinkClick);
+  });
 }
 
 /**
  * Function to show selected section
  */
-function showSection(id) {
-    const section = document.querySelector(id);
+function showSection(href) {
+  const targetSection = document.querySelector(href);
+  if (targetSection) {
+    targetSection.style.display = "grid";
+  }
+}
+
+/**
+ * Function to show all calculator sections
+ */
+function showAllCalculators() {
+  const calculators = [
+    "#vst",
+    "#fuel-consumption",
+    "#currency-calculator",
+    "#co2-footprint",
+  ];
+  calculators.forEach((calculator) => {
+    const section = document.querySelector(calculator);
     if (section) {
-        section.style.display = "grid";
+      section.style.display = "grid";
     }
+  });
+}
+
+// Initialize the modal menu
+let modalMenu = initializeModalMenu();
+// variable to store the last clicked link
+let lastClickedLink = null;
+
+/**
+ * Function to handle the click event on the navigation links
+ */
+function handleNavLinkClick(event) {
+  const href = this.querySelector("a").getAttribute("href");
+  lastClickedLink = href;
+  if (href === "index.html" || href === "#instructions") {
+    if (href === "index.html") {
+      location.reload();
+    } else {
+      event.preventDefault();
+      hideAllSections();
+      showSection(href);
+    }
+  }
+  closeModal(modalMenu.modal, modalMenu.blurContent);
+  updateNavLinks();
 }
 
 /**
  * Function to update the navigation links based on screen size
  */
 function updateNavLinks() {
-    // Check if screen size is larger then 992px
-    if (window.innerWidth >= 992) {
-        // If equal or bigger, show only "instructions" and "index" links
-        overlayNavLinks.forEach((link) => {
-            const showLinks = link.getAttribute("href");
-            if (showLinks === "#instructions" || showLinks === "index.html") {
-                link.style.display = "block";
-            } else {
-                link.style.display = "none";
-            }
-        });
-    } else {
-        // If smaller, set href of navigation their original values
-        navLinks.forEach((link) => {
-            link.style.display = "block";
-        });
-    }
+  const screenWidth = window.innerWidth;
+  if (screenWidth >= 992) {
+    handleLargeScreen();
+  } else {
+    handleSmallScreen();
+  }
 }
 
-// Add click event listener to each overlay-nav-link
-navLinks.forEach((link) => {
+/**
+ * Function to handle instruction section links
+ */
+function handleInstructionLinks() {
+  const navLinks = document.querySelectorAll(".overlay-nav-links li");
+
+  navLinks.forEach((link) => {
+    const href = link.querySelector("a").getAttribute("href");
+    if (href === "#instructions") {
+      link.style.display = "block";
+    } else {
+      link.style.display = "none";
+    }
+  });
+}
+
+/**
+ * Function to handle navigation on large screens
+ */
+function handleLargeScreen() {
+  handleInstructionLinks();
+  const navLinks = document.querySelectorAll(".overlay-nav-links li");
+  navLinks.forEach((link) => {
+    const href = link.querySelector("a").getAttribute("href");
+    if (href !== "index.html" && href !== "#instructions") {
+      link.style.display = "none";
+    } else {
+      link.style.display = "block";
+    }
+  });
+
+  // Check if the instructions section is open
+  const instructionSection = document.getElementById("instructions");
+  if (instructionSection && instructionSection.style.display !== "grid") {
+    showAllCalculators();
+  }
+}
+
+/**
+ * Function to handle navigation on small screens
+ */
+function handleSmallScreen() {
+  const navLinks = document.querySelectorAll(".overlay-nav-links li");
+
+  navLinks.forEach((link) => {
+    link.style.display = "block";
+  });
+
+  if (lastClickedLink) {
+    hideAllSections();
+    showSection(lastClickedLink);
+  }
+}
+
+/**
+ * Function to handle clicks on instruction links
+ */
+function handleInstructionLinkClicks() {
+  const instructionLinks = document.querySelectorAll(".instructions-wrapper a");
+  instructionLinks.forEach((link) => {
     link.addEventListener("click", function (event) {
-        const href = this.getAttribute("href");
-        // Check if the clicked link href is "index.html" or "#instructions"
-        if (href === "index.html" || href === "#instructions") {
-            if (href === "index.html") {
-                // If it is, reload page for index.html
-                location.reload();
-            } else {
-                // Prevent default action for "#instructions"
-                event.preventDefault();
-
-                // Hide all sections
-                hideAllSections();
-
-                // Show the selected section
-                showSection(href);
-
-                // Hide the modal menu
-                document.getElementById("modal-menu").style.display = "none";
-
-                // Reset blur effect to content behind modal menu to hidden
-                document.getElementById("blur-content").classList.remove("active");
-            }
+      event.preventDefault();
+      const href = link.getAttribute("href");
+      if (window.innerWidth < 992) {
+        hideAllSections();
+        showSection(href);
+      } else {
+        // Close the instructions section
+        const instructionSection = document.getElementById("instructions");
+        if (instructionSection) {
+          instructionSection.style.display = "none";
         }
+        showAllCalculators();
+      }
     });
-});
-
-// Call updateNavLinks when the window is resized
-window.addEventListener("resize", updateNavLinks);
-
-// Call updateNavLinks on page load
-window.addEventListener("load", updateNavLinks);
+  });
+}
